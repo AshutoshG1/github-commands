17d3fd846265bd96d9b9b4c5559b0bfbcd46a3e2
AshutoshG1 <53992254+AshutoshG1@users.noreply.github.com>
2025-02-04 00:19:18 +0530

    Update Main.java

--- src/p1/Main.java
+++ src/p1/Main.java
@@ -3,5 +3,6 @@ package p1;
 public class Main {
     public static void main(String[] args) {
         System.out.println("We have just started learning git and github commands ");
+         System.out.println("i have added second line ");
     }
 }
